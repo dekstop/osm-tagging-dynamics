923a35eda431f2e6a5e18e7b9a0a0bc070400ea5
martind <martin@dekstop.de>
2013-07-15 11:48:19 +0100

    schema updates

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -21,19 +21,39 @@ DROP TABLE poi_tag;
 CREATE TABLE poi_tag (
   id          SERIAL PRIMARY KEY,
   poi_id      INTEGER NOT NULL,
-  poi_version INTEGER NOT NULL,
+  version     INTEGER NOT NULL,
   key         CHARACTER VARYING,
   value       CHARACTER VARYING
 );
 
+CREATE INDEX idx_poi_tag_poi_id_version ON poi_tag(poi_id, version);
 CREATE INDEX idx_poi_tag_key_value ON poi_tag(key, value);
+
+-- =========
+-- = Views =
+-- =========
+
+DROP VIEW view_poi_tag_history;
+CREATE VIEW view_poi_tag_history AS 
+  SELECT 
+    poi_id, poi.version as version,
+    changeset, timestamp, uid, username, latitude, longitude,
+    key, value
+  FROM poi JOIN poi_tag ON (poi.id=poi_tag.poi_id AND poi.version=poi_tag.version);
+
+DROP VIEW view_poi_tag_firstversion;
+CREATE VIEW view_poi_tag_firstversion AS
+  SELECT min(id) AS poi_tag_id 
+  FROM poi_tag 
+  GROUP BY poi_id, key, value 
+  ORDER BY poi_tag_id;
 
 -- ===============
 -- = Derivatives =
 -- ===============
 
-DROP TABLE poi_tag_first_version;
-CREATE TABLE poi_tag_first_version (
-  poi_tag_id  INTEGER NOT NULL,
-  UNIQUE(poi_tag_id)
-);
+-- DROP TABLE poi_tag_first_version;
+-- CREATE TABLE poi_tag_first_version (
+--   poi_tag_id  INTEGER NOT NULL,
+--   UNIQUE(poi_tag_id)
+-- );
