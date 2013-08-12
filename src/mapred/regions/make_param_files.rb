5892685ce6267780153e478bfc3bb71f43119d22
martind <martin@dekstop.de>
2013-08-12 19:49:42 +0100

    Pig region parameter files, generated by a simple ruby script from TSV

---
+++ src/mapred/regions/make_param_files.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+SCRIPT_DIR = File.dirname(__FILE__)
+TEMPLATE = File.read("#{SCRIPT_DIR}/TEMPLATE.grunt")
+
+File.read("#{SCRIPT_DIR}/regions.txt").each_line do |line|
+  cols = line.chomp.split("\t")
+  name = cols[0]
+  puts "#{name}..."
+  File.open("#{SCRIPT_DIR}/region_#{name}.grunt", 'w') do |f|
+    TEMPLATE.each_line do |tl|
+      cols.each_index do |idx|
+        tl = tl.gsub("{{#{idx}}}", cols[idx])
+      end
+      f << tl
+    end
+  end
+end
