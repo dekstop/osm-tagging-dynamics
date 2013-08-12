3a50fc2fbaa99f0c78556facbeb401aedf5b833c
martind <martin@dekstop.de>
2013-08-12 19:59:27 +0100

    geofilter launcher

---
+++ src/mapred/geofilter_all_regions.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
+
+echo "Making region parameter files..."
+ruby "${SCRIPT_DIR}"/regions/make_param_files.rb
+
+echo "Running Pig jobs..."
+for regionfile in "${SCRIPT_DIR}"/regions/region_*.grunt
+do
+  echo $regionfile
+  pig -param_file $regionfile "${SCRIPT_DIR}"/geofilter.grunt -stop_on_failure || exit 1
+done
