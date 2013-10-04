4e2593c35c49144c54fdaf29cbca366668491505
martind <martin@dekstop.de>
2013-10-04 17:37:18 +0100

    percentile stored procedure

--- src/sql/functions.sql
+++ src/sql/functions.sql
@@ -20,3 +20,48 @@ CREATE AGGREGATE median(numeric) (
   FINALFUNC=_final_median,
   INITCOND='{}'
 );
+
+
+-- http://stackoverflow.com/a/14309370
+-- Usage: SELECT percentile_cont(array_agg(my_sort_column), 0.25) as my_result_column FROM ...
+
+CREATE OR REPLACE FUNCTION array_sort (ANYARRAY)
+RETURNS ANYARRAY LANGUAGE SQL
+AS $$
+  SELECT ARRAY(
+      SELECT $1[s.i] AS "foo"
+      FROM
+          generate_series(array_lower($1,1), array_upper($1,1)) AS s(i)
+      ORDER BY foo
+  );
+$$;
+
+CREATE OR REPLACE FUNCTION percentile_cont(myarray real[], percentile real)
+  RETURNS real AS
+$$
+  DECLARE
+    ary_cnt INTEGER;
+    row_num real;
+    crn real;
+    frn real;
+    calc_result real;
+    new_array real[];
+  BEGIN
+    ary_cnt = array_length(myarray,1);
+    row_num = 1 + ( percentile * ( ary_cnt - 1 ));
+    new_array = array_sort(myarray);
+
+    crn = ceiling(row_num);
+    frn = floor(row_num);
+
+    if crn = frn and frn = row_num then
+      calc_result = new_array[row_num];
+    else
+      calc_result = (crn - row_num) * new_array[frn] 
+              + (row_num - frn) * new_array[crn];
+    end if;
+
+    RETURN calc_result;
+  END;
+$$
+LANGUAGE 'plpgsql' IMMUTABLE;
