883528123815e2b9aab26419740193f737bc9a52
martind <martin@dekstop.de>
2013-11-06 15:11:21 +0000

    fetch

--- etl/bin/fetch_osh.sh
+++ etl/bin/fetch_osh.sh
@@ -15,10 +15,10 @@ oshDir=${OSH_DATADIR}/continents-${tag}
 echo "Storing files in ${oshDir}"
 mkdir -p ${oshDir} > /dev/null 2>&1
 
-for name in files
+for name in ${files}
 do
   url=${rootUrl}/${name}.osh.pbf
   dest=${oshDir}/${name}.osh.pbf
   echo "Fetching ${url} ..."
-  curl ${url} -o ${dest}
+  curl ${url} -o ${dest} || exit 1
 done
