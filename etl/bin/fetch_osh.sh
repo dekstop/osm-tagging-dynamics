a8e49d20f18d8b1aaeb2d9fe99fe0aa358e757ed
martind <martin@dekstop.de>
2013-11-06 15:07:47 +0000

    fecth

---
+++ etl/bin/fetch_osh.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BIN=$( cd "$( dirname "$0" )" && pwd )
+. ${BIN}/env.sh
+
+# ========
+# = Main =
+# ========
+
+tag=2013-11-02
+rootUrl=http://osm.personalwerk.de/full-history-extracts/planet-history-${tag}/continents
+files="africa antarctica asia australia-oceania central-america europe north-america south-america"
+
+oshDir=${OSH_DATADIR}/continents-${tag}
+echo "Storing files in ${oshDir}"
+mkdir -p ${oshDir} > /dev/null 2>&1
+
+for name in files
+do
+  url=${rootUrl}/${name}.osh.pbf
+  dest=${oshDir}/${name}.osh.pbf
+  echo "Fetching ${url} ..."
+  curl ${url} -o ${dest}
+done
