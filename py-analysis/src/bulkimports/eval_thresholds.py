1e3157a0f33318f2a9e0230b00181ec176a381bb
martind <martin@dekstop.de>
2014-02-01 18:42:00 +0000

    relative thresholds

--- py-analysis/src/bulkimports/eval_thresholds.py
+++ py-analysis/src/bulkimports/eval_thresholds.py
@@ -6,8 +6,11 @@ from __future__ import division # non-truncating division in Python 2.x
 
 import argparse
 from collections import defaultdict
+import decimal
 import random
 import sys
+
+import numpy
 
 from app import *
 
@@ -30,15 +33,14 @@ def eval_report(data, colnames, outdir, filename_base):
     outcsv.writerow([encode(row[colname]) for colname in colnames])
   outfile.close()
 
-# =========
-# = Tools =
-# =========
+# ======
+# = DB =
+# ======
 
-def f_score(precision, recall, beta=1):
-  return (1 + beta*beta) * (precision*recall) / (beta*beta*precision + recall)
-
+# Global only for now.
+# abs_threshold: min. number of edits
 # Returns a set of uids
-def get_uids_above_threshold(session, abs_threshold):
+def get_uids_above_abs_threshold(session, abs_threshold):
   result = session.execute("""SELECT uid 
     FROM user_edit_stats ue 
     GROUP BY uid 
@@ -48,6 +50,40 @@ def get_uids_above_threshold(session, abs_threshold):
     uids.add(row['uid'])
   return uids
   
+# Global only for now.
+# percentile: [0..100] inclusive
+# Returns an absolute threshold
+def get_threshold_for_percentile(session, percentile):
+  ranking = []
+  result = session.execute("""SELECT num_edits 
+    FROM user_edit_stats 
+    ORDER BY num_edits ASC""")
+  for row in result:
+    ranking.append(row['num_edits'])
+  return round(numpy.percentile(ranking, percentile))
+  
+# =========
+# = Tools =
+# =========
+
+def f_score(precision, recall, beta=1):
+  return (1 + beta*beta) * (precision*recall) / (beta*beta*precision + recall)
+
+def eval_summary(cohort, filter_type, filter_param, abs_threshold, relevant, retrieved):
+  rec = defaultdict(lambda: '-')
+  rec['cohort'] = cohort
+  rec['filter_type'] = filter_type
+  rec['filter_param'] = filter_param
+  rec['abs_threshold'] = abs_threshold
+  rec['num_relevant'] = len(relevant)
+  rec['num_retrieved'] = len(retrieved)
+  rec['num_relevant_retrieved'] = len(relevant.intersection(retrieved))
+  if rec['num_retrieved']>0:
+    rec['precision'] = float(rec['num_relevant_retrieved']) / rec['num_retrieved']
+    rec['recall'] = float(rec['num_relevant_retrieved']) / rec['num_relevant']
+    rec['F_1'] = f_score(rec['precision'], rec['recall'], 1)
+    rec['F_0_5'] = f_score(rec['precision'], rec['recall'], 0.5)
+  return rec
 
 # =========
 # = Plots =
@@ -108,31 +144,31 @@ if __name__ == "__main__":
   #
 
   # report TSV format
-  colnames = ['region', 'filtertype', 'threshold', 'num_relevant', 'num_retrieved',
-    'num_relevant_retrieved', 'precision', 'recall', 'F_1', 'F_0_5']
+  colnames = ['cohort', 'filter_type', 'filter_param', 'abs_threshold', 
+    'num_relevant', 'num_retrieved', 'num_relevant_retrieved', 'precision', 
+    'recall', 'F_1', 'F_0_5']
 
   # a list of evaluation metrics per filter and threshold
-  data = []
+  eval_stats = []
   
-  for abs_threshold in range(1000, 50000, 1000):
+  for threshold in range(1000, 50000, 1000):
     # global
     relevant = all_users['bulkimport'] # .union(all_users['unknown'])
-    retrieved = get_uids_above_threshold(session, abs_threshold)
-    
-    rec = dict()
-    rec['region'] = 'global'
-    rec['filtertype'] = 'absolute'
-    rec['threshold'] = abs_threshold
-    rec['num_relevant'] = len(relevant)
-    rec['num_retrieved'] = len(retrieved)
-    rec['num_relevant_retrieved'] = len(relevant.intersection(retrieved))
-    rec['precision'] = float(rec['num_relevant_retrieved']) / rec['num_retrieved']
-    rec['recall'] = float(rec['num_relevant_retrieved']) / rec['num_relevant']
-    rec['F_1'] = f_score(rec['precision'], rec['recall'], 1)
-    rec['F_0_5'] = f_score(rec['precision'], rec['recall'], 0.5)
-    
-    data.append(rec)
-    
+    retrieved = get_uids_above_abs_threshold(session, threshold)
+    eval_stats.append(eval_summary('global', 
+      'absolute', threshold, threshold, 
+      relevant, retrieved))
+  
+  # from 40% - ~99.994% in decreasing step sizes
+  for percentile in [100 - 60.0/pow(2,n) for n in range(14)]:
+    # global
+    relevant = all_users['bulkimport'] # .union(all_users['unknown'])
+    abs_threshold = get_threshold_for_percentile(session, percentile)
+    retrieved = get_uids_above_abs_threshold(session, abs_threshold)
+    eval_stats.append(eval_summary('global', 
+      'relative', percentile, abs_threshold, 
+      relevant, retrieved))
+
     # by country
     # for iso2 in sorted(by_country.keys()):
       # pass
@@ -143,5 +179,5 @@ if __name__ == "__main__":
   # 
 
   mkdir_p(args.outdir)
-  eval_report(data, colnames, args.outdir, 'global_thresholds')
+  eval_report(eval_stats, colnames, args.outdir, 'global_thresholds')
   
