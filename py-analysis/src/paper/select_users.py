da775021bc939434b37ac8d40a88d5f03a12d6b9
martind <martin@dekstop.de>
2014-03-28 12:32:53 +0000

    refactor: shared functions module

--- py-analysis/src/paper/select_users.py
+++ py-analysis/src/paper/select_users.py
@@ -18,6 +18,7 @@ from matplotlib import ticker
 import numpy as np
 
 from app import *
+from shared import *
 
 # =========
 # = Tools =
@@ -30,57 +31,6 @@ def get_unknown_countries(session, iso2_codes):
   found = set([rec['iso2'] for rec in result])
   return [iso2 for iso2 in iso2_codes if iso2 not in found]
  
-# ===========
-# = Reports =
-# ===========
-
-# data: a dict: key -> list of dictionaries
-def group_report(data, keycolname, valcolnames, outdir, filename_base):
-  filename = "%s/%s.txt" % (outdir, filename_base)
-  outfile = open(filename, 'wb')
-  outcsv = csv.writer(outfile, dialect='excel-tab')
-  outcsv.writerow([keycolname] + valcolnames)
-  for key in sorted(data.keys()):
-    for row in data[key]:
-      outcsv.writerow([key] + [row[colname] for colname in valcolnames])
-  outfile.close()
-
-# data: a nested dict: key -> dict
-def segment_report(data, keycolname, segcolnames, outdir, filename_base):
-  filename = "%s/%s.txt" % (outdir, filename_base)
-  outfile = open(filename, 'wb')
-  outcsv = csv.writer(outfile, dialect='excel-tab')
-  outcsv.writerow([keycolname] + segcolnames)
-  for key in sorted(data.keys()):
-    outcsv.writerow([key] + [data[key][colname] for colname in segcolnames])
-  outfile.close()
-
-# ==========
-# = Graphs =
-# ==========
-
-# data: iso2 -> measure -> value
-# Plots percentage thresholds, where values in range [0..1]
-# kwargs is passed on to plt.bar(...).
-def group_share_plot(data, iso2s, measures, outdir, filename_base, 
-  colors=QUALITATIVE_MEDIUM, scale=100, **kwargs):
-
-  for (measure, iso2, ax1) in plot_matrix(measures, iso2s, cellwidth=4, cellheight=0.5):
-    colgen = looping_generator(colors)
-    value = data[iso2][measure] * scale
-    ax1.barh(0, value, 1, left=0, color=next(colgen), **kwargs)
-    ax1.barh(0, scale-value, 1, left=value, color=next(colgen), **kwargs)
-
-    ax1.margins(0, 0)
-    # ax1.get_xaxis().set_major_formatter(ticker.FuncFormatter(to_even_percent))
-    ax1.get_xaxis().set_ticks([])
-    ax1.get_yaxis().set_ticks([])
-    
-    ax1.patch.set_visible(False)
-  
-  plt.savefig("%s/%s.pdf" % (outdir, filename_base), bbox_inches='tight')
-  plt.savefig("%s/%s.png" % (outdir, filename_base), bbox_inches='tight')
-
 # ========
 # = Main =
 # ========
