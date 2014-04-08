b78567b2c676bed20355a1e771e75e62aa3fef7a
martind <martin@dekstop.de>
2014-04-08 17:09:34 +0100

    migrations

--- py-analysis/src/paper/sql/migrate_02_filter_1k.sql
+++ py-analysis/src/paper/sql/migrate_02_filter_1k.sql
@@ -1,24 +1,24 @@
 -- Implements a bulkimport filter. Creates a dedicated schema for it first.
 -- 2014-04-08 15:22:21
 
--- CREATE SCHEMA filter_1k;
--- 
--- CREATE TABLE filter_1k.poi AS 
---   SELECT p.* FROM poi p
---   JOIN changeset c ON p.changeset=c.id
---   WHERE c.num_edits<=1000;
--- VACUUM ANALYZE filter_1k.poi;
--- 
--- CREATE TABLE filter_1k.poi_tag AS 
---   SELECT p.* FROM poi_tag p
---   JOIN filter_1k.poi f ON (f.id=p.poi_id and f.version=p.version);
--- VACUUM ANALYZE filter_1k.poi_tag;
--- 
--- CREATE TABLE filter_1k.poi_tag_edit_action AS 
---   SELECT p.* FROM poi_tag_edit_action p
---   JOIN filter_1k.poi f ON (f.id=p.poi_id and f.version=p.version);
--- VACUUM ANALYZE filter_1k.poi_tag_edit_action;
--- 
+CREATE SCHEMA filter_1k;
+
+CREATE TABLE filter_1k.poi AS 
+  SELECT p.* FROM poi p
+  JOIN changeset c ON p.changeset=c.id
+  WHERE c.num_edits<=1000;
+VACUUM ANALYZE filter_1k.poi;
+
+CREATE TABLE filter_1k.poi_tag AS 
+  SELECT p.* FROM poi_tag p
+  JOIN filter_1k.poi f ON (f.id=p.poi_id and f.version=p.version);
+VACUUM ANALYZE filter_1k.poi_tag;
+
+CREATE TABLE filter_1k.poi_tag_edit_action AS 
+  SELECT p.* FROM poi_tag_edit_action p
+  JOIN filter_1k.poi f ON (f.id=p.poi_id and f.version=p.version);
+VACUUM ANALYZE filter_1k.poi_tag_edit_action;
+
 CREATE TABLE filter_1k.world_borders_poi_latest AS 
   SELECT p.* FROM world_borders_poi_latest p
   JOIN filter_1k.poi f ON (f.id=p.poi_id);
